d9af87963a88848072ea56b1600c15b1a397a829
Vibhor Singh <vibdaddy12@gmail.com>
2021-04-15 15:01:32 +0200

    Update Colourizer.cs

--- Xamarin.RGB/Colourizer.cs
+++ Xamarin.RGB/Colourizer.cs
@@ -6,5 +6,15 @@ namespace Xamarin.RGB
 {
     public class Colourizer : IColourizer
     {
+        public int Speed { get; set; } = 100;
+        public double Hue { get; set; } = 0f;
+        public double Saturation { get; set; } = 1f;
+        public double Luminosity { get; set; } = 0.5f;
+        public double Alpha { get; set; } = 1f;
+
+        public void UpdateColor()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
