95ef3d31766c456b9e99c9c478a615bfc434653c
Vibhor Singh <vibdaddy12@gmail.com>
2021-04-15 15:10:47 +0200

    Implemented stuff

--- Xamarin.RGB/Colourizer.cs
+++ Xamarin.RGB/Colourizer.cs
@@ -6,12 +6,15 @@ namespace Xamarin.RGB
 {
     public class Colourizer : IColourizer
     {
+        public CycleStyle CycleStyle { get; set; } = CycleStyle.Forwards;
+        public ChangeTypes ChangeFlages { get; set; } = ChangeTypes.Hue;
+        public ColourizerTypes ColourizerFlags { get; set; } = ColourizerTypes.BackgroundColor;
+
         public int Speed { get; set; } = 100;
         public double Hue { get; set; } = 0f;
         public double Saturation { get; set; } = 1f;
         public double Luminosity { get; set; } = 0.5f;
         public double Alpha { get; set; } = 1f;
-
         public void UpdateColor()
         {
             throw new NotImplementedException();
--- Xamarin.RGB/IColourizer.cs
+++ Xamarin.RGB/IColourizer.cs
@@ -6,6 +6,10 @@ namespace Xamarin.RGB
 {
     public interface IColourizer
     {
+        public CycleStyle CycleStyle { get; set; }
+        public ChangeTypes ChangeFlages { get; set; }
+        public ColourizerTypes ColourizerFlags { get; set; }
+
         public int Speed { get; set; }
         public double Hue { get; set; }
         public double Saturation { get; set; }
