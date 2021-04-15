450ef5abe4271461f371c275b97e7ba8e1a2d52f
Vibhor Singh <vibdaddy12@gmail.com>
2021-04-15 14:50:44 +0200

    offsets

--- Xamarin.RGB/ColouredElement.cs
+++ Xamarin.RGB/ColouredElement.cs
@@ -9,14 +9,26 @@ namespace Xamarin.RGB
 {
     public class ColouredElement : ContentView
     {
-        public ColouredElement()
+        private readonly VisualElement _element;
+
+        public ColouredElement(VisualElement element)
         {
-            Content = new StackLayout
-            {
-                Children = {
-                    new Label { Text = "Welcome to Xamarin.Forms!" }
-                }
-            };
+            _element = element;
         }
+
+        public Offset BackgroundOffset { get; set; }
+        public Color BackgroundColor { 
+            get { return _element.BackgroundColor; }
+            set { 
+                var hue = value.Hue + BackgroundOffset.HueOffset;
+                var saturation = value.Saturation + BackgroundOffset.SaturationOffset;
+                var luminosity = value.Luminosity + BackgroundOffset.LuminosityOffset;
+                var alpha = value.A + BackgroundOffset.AlphaOffset;
+                _element.BackgroundColor = Color.FromHsla(hue, saturation, luminosity, alpha);
+            }
+        }
+
+
+
     }
 }
---
+++ Xamarin.RGB/Offset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.RGB
+{
+    public struct Offset
+    {
+        public double HueOffset { get; set; }
+        public double SaturationOffset { get; set; }
+        public double LuminosityOffset { get; set; }
+        public double AlphaOffset { get; set; }
+
+        public Offset(double hueOffset, double saturationOffset, double luminosityOffset, double alphaOffset)
+        {
+            HueOffset = hueOffset;
+            SaturationOffset = saturationOffset;
+            LuminosityOffset = luminosityOffset;
+            AlphaOffset = alphaOffset;
+        }
+    }
+}
